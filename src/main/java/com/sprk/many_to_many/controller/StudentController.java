96a0e658a0b7df15076c8bf0a3d4922e0cc0ba47
Blackshadow2492 <atuldhawale5678@gmail.com>
2024-11-06 19:43:33 +0530

    create many to many mapping

---
+++ src/main/java/com/sprk/many_to_many/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.sprk.many_to_many.controller;
+
+import com.sprk.many_to_many.dto.StudentDto;
+import com.sprk.many_to_many.entity.Student;
+import com.sprk.many_to_many.mapper.StudentMapper;
+import com.sprk.many_to_many.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @PostMapping("/save")
+    public StudentDto save(@RequestBody StudentDto studentDto) {
+        Student student = StudentMapper.mappedStudentDto(studentDto,new Student());
+
+        studentRepository.save(student);
+        studentDto.setRollNo(student.getRollNo());
+        return studentDto;
+    }
+
+    @GetMapping("/get-student/{rollNo}")
+    public StudentDto getStudentByRollNo(@PathVariable int rollNo) {
+        Student student = studentRepository.findById(rollNo).orElseThrow(() -> new RuntimeException("Student Not Found"));
+
+        StudentDto studentDto = StudentMapper.mappedStudent(student,new StudentDto());
+        return studentDto;
+    }
+}
